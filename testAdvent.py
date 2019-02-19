a8f868db97bb55ab7c075546d1d35b368993fe9f
lyl-slitherers <47189854+lyl-slitherers@users.noreply.github.com>
2019-02-19 13:35:24 -0500

    Add files via upload

---
+++ testAdvent.py
@@ -0,0 +1,9 @@
+print("December 26th, 2240")
+print("")
+print("You are Wolfgang Steinway, making your way across the galaxy in an Intergalactic battleship searching for any signs of life.")
+print("You are evading a nuclear war on Earth, barely escaping a Tsar bomba approaching your home town - Berlin.")
+print("Years of global warming has risen the sea levels
+
+x = input("Enter 1 to walk towards it. Enter 2 to run away.")
+if (x == "1"):
+    print("You slowly approach the noise and see the shadow of a four legged creature")
