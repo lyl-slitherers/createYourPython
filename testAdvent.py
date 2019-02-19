7773aefec925c6d6c162be0edecb6c1f5e76a223
Ty Gutowski <tygutowski@gmail.com>
2019-02-19 13:40:14 -0500

    Update testAdvent.py

--- testAdvent.py
+++ testAdvent.py
@@ -2,10 +2,10 @@ print("December 26th, 2240")
 print("")
 print("You are Wolfgang Steinway, making your way across the galaxy in an Intergalactic battleship searching for any signs of life.")
 print("You are evading a nuclear war on Earth, barely escaping a Tsar bomba approaching your home town - Berlin.")
-print("Years of global warming has risen the sea levels
+print("Years of global warming has risen the sea levels several feet, causing the majority of the coasts to be flooded, leaving them empty.")
 
-print("1. Walk towards the creature")
+print("\n1. Walk towards the creature")
 print("2. Slowly back away")
-x = input
+x = input("\n")
 if (x == "1"):
     print("You slowly approach the noise and see the shadow of a four legged creature")
